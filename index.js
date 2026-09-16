831ad002944f6a3c912912b648de0bf8f0bcf70b
rodac11 <r.aguilera.croasdaile@gmail.com>
2026-09-15 21:46:11 -0600

    add cors, update README

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 const path = require('path')
+const cors = require('cors')
 const app = express()
 
 const linkSchema = mongoose.Schema({
