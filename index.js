f20fdf7389adaa4e198688922f5430e13e1c4bb7
rodac11 <r.aguilera.croasdaile@gmail.com>
2026-09-15 21:08:05 -0600

    feat: add get by ID, post, put, delete

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const linkSchema = mongoose.Schema({
     note: String,
 })
 
+
+linkSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+	returnedObject.id = returnedObject._id.toString()
+	delete returnedObject._id
+	delete returnedObject.__v
+    }
+    })
+
+
 const Link = mongoose.model('Link', linkSchema)
 
 const mongoUrl = process.env.MONGODB_URI
@@ -27,17 +37,89 @@ app.get('/', (req, res) => {
 app.get('/api/links', (req, res) => {
     Link.find({}).then((links) => {
 	res.json(links)
-	console.log(links)
+	//console.log(links)
 	})
 })
 
 //get 1 by ID
+app.get('/api/links/:id', (req, res) => {
+    console.log(req.params)
+    Link.findById(req.params.id).then((link) => {
+	if (link) {
+	    res.json(link)
+	} else {
+	    console.log('not found not found')
+	    res.status(404).end()
+	}
+    })
+})
+
 
 //post
+app.post('/api/links', (req, res, next) => {
+    const body = req.body
+    if(!body) {
+	return res.status(400).json({
+	    error: 'nothing to post',
+	})
+    }
+    if (!body.name) {
+	return res.status(400).json({
+	    error: 'missing name'
+	})
+    }
+    if (!body.url) {
+	return res.status(400).json({
+	    error: 'missing url'
+	})
+    }
+    if (!body.note) {
+	body.note = ''
+    }
+    //unique name? unique url?
+    const link = new Link({
+	name: body.name,
+	url: body.url,
+	note: body.note,
+    })
+
+    link.save().then(savedLink => {
+	res.json(savedLink)
+	console.log("POST")
+    })
+	.catch(error => next(error))
+})
 
 //put
+app.put('/api/links/:id', (req, res, next) => {
+    const body = req.body
 
-//delete
+    Link.findById(req.params.id)
+	.then((link) => {
+	    if(!link) {
+		return res.status(404).end()
+	    }
+
+	    link.name = body.name
+	    link.url = body.url
+	    link.note = body.note
+
+	    return link.save().then((updatedLink) => {
+		res.json(updatedLink)
+	    })
+	})
+	.catch((error) => next(error))
+})
+	    
+
+
+app.delete('/api/links/:id', (req, res, next) => {
+    Link.findByIdAndDelete(req.params.id)
+	.then(
+	    res.status(204).end()
+	)
+	.catch(error => next (error))
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
